92255d116e0b106908ec14f96bf5af02b66b547f
sectarian <matthew.poulter0@gmail.com>
2016-04-20 00:09:53 +0100

    Added Functions to library, thinned phonelist.JS considerably

--- phonelist.js
+++ phonelist.js
@@ -1,174 +1,116 @@
-// Contact Class
-function Contact(fn, sn, no, em, of) {
-    this.FirstName = fn;
-    this.SecondName = sn;
-    this.internal = no;
-    this.external = em;
-    this.office = of;
-    this.hidden = false;
-}
-
-// Address Book Array
 var addressBook = new Array();
-
-// Search Results Array
 var searchResults = new Array();
 
-// Populate contact book
-function populate(){
-    var contactRows = gbc("tableRow");
-    while(contactRows[0]){
-        contactRows[0].parentNode.removeChild(contactRows[0]);        
-    }
-    
-    
-    for(var i = 0; i < addressBook.length; i++){  
-            gbi("table").insertRow(-1);
-            var rows = gbi("table").rows;
-            var lastRow = rows[rows.length - 1];
-            lastRow.className += " tableRow";
-            lastRow.insertCell(0);
-            lastRow.insertCell(1);
-            lastRow.insertCell(2);
-            lastRow.insertCell(3);
-            lastRow.insertCell(4);
-
-
-             for (var key in addressBook[i]){ 
-               if(key == "FirstName"){
-                    lastRow.cells[0].innerHTML = caps(addressBook[i][key]);
-                } else if(key == "SecondName"){
-                    lastRow.cells[1].innerHTML = caps(addressBook[i][key]);
-                } else if(key == "internal"){
-                    lastRow.cells[2].innerHTML = addressBook[i][key];
-                } else if(key == "external"){
-                    lastRow.cells[3].innerHTML = addressBook[i][key];
-                } else if(key == "office"){
-                    lastRow.cells[4].innerHTML = addressBook[i][key];
-                } 
-            }
-        
-        if(addressBook[i]["FirstName"] == "admindeletedxfx"){
-            addressBook[i]["hidden"] = true;
-        }
-        if(addressBook[i]["hidden"] !== false){
-        gbt('tr')[i+1].style.display = "none";
-    }
-    }
-    
-    
-}
-populate();
-    
-// Sort by String
-function sortBy(key) {
-    if (key == "internal"){
-        addressBook.sort(function(a,b){
-            alc = a[key];
-            blc = b[key];            
-            return alc > blc ? 1 : alc < blc ? -1 : 0;
-        });
-        populate();
-    } else {        
-        addressBook.sort(function(a,b){
-            var alc = a[key].toLowerCase(), 
-            blc = b[key].toLowerCase();
-            return alc > blc ? 1 : alc < blc ? -1 : 0;
-        });  
-        populate();
-    }
-}
-
-// Sort by Column header Ascending/Descending
-function ascendDescend (columnHeader) {
-    if (sortedBy != columnHeader){
-    sortBy(columnHeader);
-    sortedBy = columnHeader;
-    } else {
-        addressBook.reverse();
-        populate();
-    }  
-}
-
-// Highlight
-function highlight(header) {
-    for (var i=0; i<5; i++) {
-    document.getElementById("topRow").getElementsByTagName('td')[i].setAttribute('class', "");
-    }
-    header.setAttribute('class', "highlighted");    
-}
-
-// Sort by ...
-var sortedBy = "";
-gbi("firstNameHead").addEventListener("click", function(){
-    ascendDescend("FirstName");
-    highlight(this);
-});
-gbi("secondNameHead").addEventListener("click", function(){
-    ascendDescend("SecondName");
-    highlight(this);
-});
-gbi("internalHead").addEventListener("click", function(){
-    ascendDescend("internal");
-    highlight(this);
-});
-gbi("externalHead").addEventListener("click", function(){
-    ascendDescend("external");
-    highlight(this);
-});
-gbi("office").addEventListener("click", function(){
-    ascendDescend("office");
-    highlight(this);
-});
-
-// Search
-function searchContacts(){
-    var searchBoxValue = gbi("searchBox").value.toLowerCase();
-    var searchFor = new RegExp(searchBoxValue);
-    
-    for (var i=0; i<addressBook.length; i++){
-        var match = false;
-        for(var key in addressBook[i]){
-            if(key !== "hidden"){
-                var trial = addressBook[i][key].toLowerCase();
-                var trialResult =  searchFor.test(trial);
-                if(trialResult == true){
-                    match = true;
-                }
-
-            }
-            if(match == false){
-                addressBook[i]["hidden"] = true;
-            } else {
-                addressBook[i]["hidden"] = false;
-            }
-        }
-    }
-    populate();
-}
-
-gbi('searchBox').addEventListener("input", searchContacts);
-
-// Load
-function load(){
+function loadContacts(){
     var request = new HttpRequest("contacts.txt", handleData);
     request.send();
 }
-
-load();
-
+loadContacts();
 function handleData(text) {
     var retrievedData = JSON.parse(text);
     addressBook = retrievedData;
-    ascendDescend('FirstName');
-    highlight(gbi('firstNameHead'));
-    createTableFromArray(addressBook, 'testingtable', "testingDiv");
+    searchArrayObjects('searchBox', addressBook, searchResults);
+    sortObjectArray(searchResults, 'FirstName');
+    fillTableWithArray(searchResults, 'table');
 }
 
-function sendData() {
-    var XPort = JSON.stringify(addressBook);
-    document.write(XPort)
-}
+gbi('searchBox').addEventListener("input", function(){
+    clearTable('table');
+    searchResults = [];
+    searchArrayObjects('searchBox', addressBook, searchResults);
+    fillTableWithArray(searchResults, 'table');
+});
+
+classHighlight('rowHeader', 'rowHeaderHighlighted');
+  
+//// Sort by String
+//function sortBy(key) {
+//    if (key == "internal"){
+//        addressBook.sort(function(a,b){
+//            alc = a[key];
+//            blc = b[key];            
+//            return alc > blc ? 1 : alc < blc ? -1 : 0;
+//        });
+//    } else {        
+//        addressBook.sort(function(a,b){
+//            var alc = a[key].toLowerCase(), 
+//            blc = b[key].toLowerCase();
+//            return alc > blc ? 1 : alc < blc ? -1 : 0;
+//        });  
+//    }
+//}
+//
+//// Sort by Column header Ascending/Descending
+//function ascendDescend (columnHeader) {
+//    if (sortedBy != columnHeader){
+//    sortBy(columnHeader);
+//    sortedBy = columnHeader;
+//    } else {
+//        addressBook.reverse();
+//        populate();
+//    }  
+//}
+//
+//// Highlight
+//function highlight(header) {
+//    for (var i=0; i<5; i++) {
+//    document.getElementById("topRow").getElementsByTagName('td')[i].setAttribute('class', "");
+//    }
+//    header.setAttribute('class', "highlighted");    
+//}
+//
+//// Sort by ...
+//var sortedBy = "";
+//gbi("firstNameHead").addEventListener("click", function(){
+//    ascendDescend("FirstName");
+//    highlight(this);
+//});
+//gbi("secondNameHead").addEventListener("click", function(){
+//    ascendDescend("SecondName");
+//    highlight(this);
+//});
+//gbi("internalHead").addEventListener("click", function(){
+//    ascendDescend("internal");
+//    highlight(this);
+//});
+//gbi("externalHead").addEventListener("click", function(){
+//    ascendDescend("external");
+//    highlight(this);
+//});
+//gbi("office").addEventListener("click", function(){
+//    ascendDescend("office");
+//    highlight(this);
+//});
+//
+//// Search
+//function searchContacts(){
+//    var searchBoxValue = gbi("searchBox").value.toLowerCase();
+//    var searchFor = new RegExp(searchBoxValue);
+//    
+//    for (var i=0; i<addressBook.length; i++){
+//        var match = false;
+//        for(var key in addressBook[i]){
+//            if(key !== "hidden"){
+//                var trial = addressBook[i][key].toLowerCase();
+//                var trialResult =  searchFor.test(trial);
+//                if(trialResult == true){
+//                    match = true;
+//                }
+//
+//            }
+//            if(match == false){
+//                addressBook[i]["hidden"] = true;
+//            } else {
+//                addressBook[i]["hidden"] = false;
+//            }
+//        }
+//    }
+//    populate();
+//}
+//
+//gbi('searchBox').addEventListener("input", searchContacts);
+//
+// Load
 
 
 
--- phonelistAdmin.js
+++ phonelistAdmin.js
@@ -12,161 +12,161 @@ var addressBook = new Array();
 // Search Results Array
 var searchResults = new Array();
 // Populate contact book
-function populate(){
-    var contactRows = gbc("tableRow");
-    while(contactRows[0]){
-        contactRows[0].parentNode.removeChild(contactRows[0]);        
-    }
-    
-    
-    for(var i = 0; i < exportArray.length; i++){  
-            gbi("table").insertRow(-1);
-            var rows = gbi("table").rows;
-            var lastRow = rows[rows.length - 1];
-            lastRow.className += " tableRow";
-            lastRow.insertCell(0);
-            lastRow.insertCell(1);
-            lastRow.insertCell(2);
-            lastRow.insertCell(3);
-            lastRow.insertCell(4);
-            lastRow.insertCell(5);
-         
-
-
-
-             for (var key in exportArray[i]){ 
-               if(key == "FirstName"){
-                    lastRow.cells[0].innerHTML = caps(exportArray[i][key]);
-                    lastRow.cells[0].contentEditable = "true";
-                } else if(key == "SecondName"){
-                    lastRow.cells[1].innerHTML = caps(exportArray[i][key]);
-                    lastRow.cells[1].contentEditable = "true";
-                } else if(key == "internal"){
-                    lastRow.cells[2].innerHTML = exportArray[i][key];
-                    lastRow.cells[2].contentEditable = "true";
-                } else if(key == "external"){
-                    lastRow.cells[3].innerHTML = exportArray[i][key];
-                    lastRow.cells[3].contentEditable = "true";
-                } else if(key == "office"){
-                    lastRow.cells[4].innerHTML = exportArray[i][key];
-                    lastRow.cells[4].contentEditable = "true";
-                }
-            }
-        
-        lastRow.cells[5].innerHTML = "<input type='checkbox' name='selector' class='tickMe'>";
-        if(exportArray[i]["FirstName"] == "admindeletedxfx"){
-            exportArray[i]["hidden"] = true;
-        }
-        if(exportArray[i]["hidden"] !== false){
-        gbt('tr')[i+1].style.display = "none";
-    }
-    }
-    
-    
-}
+//function populate(){
+//    var contactRows = gbc("tableRow");
+//    while(contactRows[0]){
+//        contactRows[0].parentNode.removeChild(contactRows[0]);        
+//    }
+//    
+//    
+//    for(var i = 0; i < exportArray.length; i++){  
+//            gbi("table").insertRow(-1);
+//            var rows = gbi("table").rows;
+//            var lastRow = rows[rows.length - 1];
+//            lastRow.className += " tableRow";
+//            lastRow.insertCell(0);
+//            lastRow.insertCell(1);
+//            lastRow.insertCell(2);
+//            lastRow.insertCell(3);
+//            lastRow.insertCell(4);
+//            lastRow.insertCell(5);
+//         
+//
+//
+//
+//             for (var key in exportArray[i]){ 
+//               if(key == "FirstName"){
+//                    lastRow.cells[0].innerHTML = caps(exportArray[i][key]);
+//                    lastRow.cells[0].contentEditable = "true";
+//                } else if(key == "SecondName"){
+//                    lastRow.cells[1].innerHTML = caps(exportArray[i][key]);
+//                    lastRow.cells[1].contentEditable = "true";
+//                } else if(key == "internal"){
+//                    lastRow.cells[2].innerHTML = exportArray[i][key];
+//                    lastRow.cells[2].contentEditable = "true";
+//                } else if(key == "external"){
+//                    lastRow.cells[3].innerHTML = exportArray[i][key];
+//                    lastRow.cells[3].contentEditable = "true";
+//                } else if(key == "office"){
+//                    lastRow.cells[4].innerHTML = exportArray[i][key];
+//                    lastRow.cells[4].contentEditable = "true";
+//                }
+//            }
+//        
+//        lastRow.cells[5].innerHTML = "<input type='checkbox' name='selector' class='tickMe'>";
+//        if(exportArray[i]["FirstName"] == "admindeletedxfx"){
+//            exportArray[i]["hidden"] = true;
+//        }
+//        if(exportArray[i]["hidden"] !== false){
+//        gbt('tr')[i+1].style.display = "none";
+//    }
+//    }
+//    
+//    
+//}
     
 // Sort by String
-function sortBy(key) {
-    if (key == "internal"){
-        exportArray.sort(function(a,b){
-            alc = a[key];
-            blc = b[key];            
-            return alc > blc ? 1 : alc < blc ? -1 : 0;
-        });
-        populate();
-    } else {        
-        exportArray.sort(function(a,b){
-            var alc = a[key].toLowerCase(), 
-            blc = b[key].toLowerCase();
-            return alc > blc ? 1 : alc < blc ? -1 : 0;
-        });  
-        populate();
-    }
-}
+//function sortBy(key) {
+//    if (key == "internal"){
+//        exportArray.sort(function(a,b){
+//            alc = a[key];
+//            blc = b[key];            
+//            return alc > blc ? 1 : alc < blc ? -1 : 0;
+//        });
+//        populate();
+//    } else {        
+//        exportArray.sort(function(a,b){
+//            var alc = a[key].toLowerCase(), 
+//            blc = b[key].toLowerCase();
+//            return alc > blc ? 1 : alc < blc ? -1 : 0;
+//        });  
+//        populate();
+//    }
+//}
 
 // Sort by Column header Ascending/Descending
-function ascendDescend (columnHeader) {
-    if (sortedBy != columnHeader){
-    sortBy(columnHeader);
-    sortedBy = columnHeader;
-    } else {
-        exportArray.reverse();
-        populate();
-    }  
-}
+//function ascendDescend (columnHeader) {
+//    if (sortedBy != columnHeader){
+//    sortBy(columnHeader);
+//    sortedBy = columnHeader;
+//    } else {
+//        exportArray.reverse();
+//        populate();
+//    }  
+//}
 
 // Highlight
-function highlight(header) {
-    for (var i=0; i<5; i++) {
-    document.getElementById("topRow").getElementsByTagName('td')[i].setAttribute('class', "");
-    }
-    header.setAttribute('class', "highlighted");    
-}
+//function highlight(header) {
+//    for (var i=0; i<5; i++) {
+//    document.getElementById("topRow").getElementsByTagName('td')[i].setAttribute('class', "");
+//    }
+//    header.setAttribute('class', "highlighted");    
+//}
 
 // Sort by ...
-var sortedBy = "";
-gbi("firstNameHead").addEventListener("click", function(){
-    save();
-    ascendDescend("FirstName");
-    highlight(this);
-});
-gbi("secondNameHead").addEventListener("click", function(){
-    save();
-    ascendDescend("SecondName");
-    highlight(this);
-});
-gbi("internalHead").addEventListener("click", function(){
-    save();
-    ascendDescend("internal");
-    highlight(this);
-});
-gbi("externalHead").addEventListener("click", function(){
-    save();
-    ascendDescend("external");
-    highlight(this);
-});
-gbi("office").addEventListener("click", function(){
-    save();
-    ascendDescend("office");
-    highlight(this);
-});
+//var sortedBy = "";
+//gbi("firstNameHead").addEventListener("click", function(){
+//    save();
+//    ascendDescend("FirstName");
+//    highlight(this);
+//});
+//gbi("secondNameHead").addEventListener("click", function(){
+//    save();
+//    ascendDescend("SecondName");
+//    highlight(this);
+//});
+//gbi("internalHead").addEventListener("click", function(){
+//    save();
+//    ascendDescend("internal");
+//    highlight(this);
+//});
+//gbi("externalHead").addEventListener("click", function(){
+//    save();
+//    ascendDescend("external");
+//    highlight(this);
+//});
+//gbi("office").addEventListener("click", function(){
+//    save();
+//    ascendDescend("office");
+//    highlight(this);
+//});
 
  //Search
-function searchContacts(){
-    var searchBoxValue = gbi("searchBox").value.toLowerCase();
-    var searchFor = new RegExp(searchBoxValue);
-    
-    for (var i=0; i< exportArray.length; i++){
-        var match = false;
-        for(var key in exportArray[i]){
-            if(key !== "hidden"){
-                var trial = exportArray[i][key].toLowerCase();
-                var trialResult =  searchFor.test(trial);
-                if(trialResult == true){
-                    match = true;
-                }
-
-            }
-            if(match == false){
-                exportArray[i]["hidden"] = true;
-            } else {
-                exportArray[i]["hidden"] = false;
-            }
-        }
-    }
-    populate();
-}
-gbi('searchBox').addEventListener("input", function(){
-    save();
-    searchContacts();
-});
+//function searchContacts(){
+//    var searchBoxValue = gbi("searchBox").value.toLowerCase();
+//    var searchFor = new RegExp(searchBoxValue);
+//    
+//    for (var i=0; i< exportArray.length; i++){
+//        var match = false;
+//        for(var key in exportArray[i]){
+//            if(key !== "hidden"){
+//                var trial = exportArray[i][key].toLowerCase();
+//                var trialResult =  searchFor.test(trial);
+//                if(trialResult == true){
+//                    match = true;
+//                }
+//
+//            }
+//            if(match == false){
+//                exportArray[i]["hidden"] = true;
+//            } else {
+//                exportArray[i]["hidden"] = false;
+//            }
+//        }
+//    }
+//    populate();
+//}
+//gbi('searchBox').addEventListener("input", function(){
+//    save();
+//    searchContacts();
+//});
 
 // Load
-function load(){
-    var request = new HttpRequest("contacts.txt", handleData);
-    request.send();
-}
-load();
+//function load(){
+//    var request = new HttpRequest("contacts.txt", handleData);
+//    request.send();
+//}
+//load();
 
 function handleData(text) {
     var retrievedData = JSON.parse(text);
