cd0280af9996fe6597c6378c4f5de6df8d5d2f39
sectarian <matthew.poulter0@gmail.com>
2016-09-09 17:23:43 +0100

    admin delete bug fixed

--- phonelistAdmin.js
+++ phonelistAdmin.js
@@ -73,8 +73,8 @@ function add() {
 	for (var i = 0; i < 5; i++){
 		gbi('table').rows[1].insertCell(i);
 		gbi('table').rows[1].cells[i].contentEditable = true;
-		gbi('table').rows[1].cells[i].className = "tableCells";
-	}
+		gbi('table').rows[1].cells[i].className = "tableCells";            
+    }
     gbi('table').rows[1].insertCell(i);
     gbi('table').rows[1].cells[5].className = "tableCells";
 	gbi('table').rows[1].cells[5].innerHTML = "<input type='checkbox' class='delCheck'>";
@@ -133,4 +133,10 @@ function sendData(){
 	//Must add this request header to XMLHttpRequest request for POST
 	xmlhttp.setRequestHeader("Content-type", "application/JSON");
 	xmlhttp.send(data);
+}
+
+window.onload = function() {
+ gbc('tableCells').onpaste = function(e) {
+   e.preventDefault();
+ }
 }
